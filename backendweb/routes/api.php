4ca0e2a92fb4103874e5bd9b7f15570f834e885a
juliocerna <julio.cerna@alumnos.ucn.cl>
2020-07-10 01:42:35 -0400

    feat(api): Register and login, routes added

--- backendweb/routes/api.php
+++ backendweb/routes/api.php
@@ -18,4 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/register', 'Api\AuthController@register');
+Route::post('/login', 'Api\AuthController@login');
+
 Route::apiResource('/persona', 'Api\PersonaController')->middleware('auth:api');
