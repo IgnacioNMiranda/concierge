9b200ce527edda3a4e7b6ef7184cce8da87b2344
Alex Orquera <bucket@live.cl>
2020-07-17 22:08:20 -0400

    feat: implement Registro class and table

--- backendweb/app/Registro.php
+++ backendweb/app/Registro.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Registro extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'fecha'
+    ];
+
+    public function roles()
+    {
+        return $this->belongsToMany('App\Persona');
+    }
 }
