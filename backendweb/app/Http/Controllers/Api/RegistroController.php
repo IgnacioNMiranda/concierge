f82f23560b5dab0e0f0d7607bdaee2bfcb7dcbe0
Ignacio Miranda PC <ignacio.miranda01@alumnos.ucn.cl>
2020-07-18 14:45:23 -0400

    feat: initial implementation of store method on RegistroController.php.

--- backendweb/app/Http/Controllers/Api/RegistroController.php
+++ backendweb/app/Http/Controllers/Api/RegistroController.php
@@ -1,50 +1,77 @@
-<?php
+<?php /** @noinspection PhpUndefinedMethodInspection */
 
 namespace App\Http\Controllers\Api;
 
+use App\Departamento;
 use App\Http\Controllers\Controller;
+use App\Registro;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class RegistroController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function index()
     {
-        //
+        // TODO: Missing implementation (http://conserjeria.cl/api/registro).
+        $registers = Registro::orderBy('fecha', 'ASC')->get();
+
+        //$apartment = $register->departamento()->get();
+
+        return response([
+            'registros' =>  $registers,
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return Response
      */
     public function store(Request $request)
     {
-        // TODO
+        $data = $request->all();
+
+        $register = Registro::create($data);
+
+        // Searches the apartment based on the id received on Request.
+        $apartment = Departamento::find($request->get('departamento_id'));
+
+        // Creates the relation 1-n with the apartment.
+        $register->departamento()->associate($apartment);
+        $register->save();
+
+        // Creates the relation n-n with the people that visits the apartment.
+        $register->personas()->attach($request->get('personas'));
+
+        return response([
+            'message' => "Visit registered successfully",
+            'register' => $register,
+        ]);
     }
 
     /**
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function show($id)
     {
-        //
+        // TODO (http://conserjeria.cl/api/registro/{numerodecasa/departamento}).
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function update(Request $request, $id)
     {
@@ -55,7 +82,7 @@ class RegistroController extends Controller
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function destroy($id)
     {
--- backendweb/app/Http/Resources/RegistroResource.php
+++ backendweb/app/Http/Resources/RegistroResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class RegistroResource extends JsonResource
@@ -9,11 +10,12 @@ class RegistroResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  Request  $request
      * @return array
      */
     public function toArray($request)
     {
+        /** @noinspection PhpUndefinedFieldInspection */
         return [
             'id' => $this->id,
             'fecha' => $this->fecha,
