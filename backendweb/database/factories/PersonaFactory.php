6a34a78a21cef3b76145699c288b9192fed1b3b1
juliocerna <julio.cerna@alumnos.ucn.cl>
2020-07-26 01:30:39 -0400

    feat(Persona): Creation of the files, PersonaFactory.php and PersonasTableSeeder.php

---
+++ backendweb/database/factories/PersonaFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Persona;
+use Faker\Generator as Faker;
+
+$factory->define(Persona::class, function (Faker $faker) {
+
+
+    return [
+        'rut' => $faker->unique()->regexify('[1-9]{1,2}\.[0-9]{3}\.[0-9]{3}-([0-9]|k)'),
+        'nombre' => $faker->name,
+        'telefono' => rand(00000000,99999999),
+        'email' => $faker->unique()->safeEmail,
+        'departamento_id' => $faker->unique()->numberBetween(1, 40),
+    ];
+});
