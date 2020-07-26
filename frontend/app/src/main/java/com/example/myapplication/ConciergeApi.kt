574ea914c4a714d23cbe2230bdeaa366a5f87a2d
Alex Orquera <bucket@live.cl>
2020-07-25 21:47:09 -0400

    fix(api): change return types to Response

--- frontend/app/src/main/java/com/example/myapplication/ConciergeApi.kt
+++ frontend/app/src/main/java/com/example/myapplication/ConciergeApi.kt
@@ -2,7 +2,6 @@ package com.example.myapplication
 
 import okhttp3.ResponseBody
 import retrofit2.Response
-import retrofit2.Call
 import retrofit2.http.*
 
 /**
@@ -16,49 +15,49 @@ interface ConciergeApi {
     /**
      * Get a list of all [Registro]s.
      *
-     * @return a [Call] that can return a [Response] with a Collection of [Registro].
+     * @return a [Response] with a Collection of [Registro].
      */
     @GET("/api/registro")
-    suspend fun registros(): Call<Collection<Registro>>
+    suspend fun registros(): Response<Collection<Registro>>
 
     /**
      * Create a [Registro].
      *
      * @param id The ID of the [Registro]
-     * @return a [Call] that can return a [Response] with a [Registro].
+     * @return a [Response] with a [Registro].
      */
     @POST("/api/registro")
-    suspend fun createRegistro(@Body registro: Registro): Call<Registro>
+    suspend fun createRegistro(@Body registro: Registro): Response<Registro>
 
     /**
      * Get a [Registro] with its associated ID of [Departamento].
      *
      * @param id The ID of the [Departamento].
-     * @return a [Call] that can return a [Response] with a [Registro].
+     * @return a [Response] with a [Registro].
      */
     @GET("/api/registro/{departamento_id}")
-    suspend fun readRegistro(@Path("departamento_id") id: Long): Call<Registro>
+    suspend fun readRegistro(@Path("departamento_id") id: Long): Response<Registro>
 
     /**
      * Update a [Registro].
      *
      * @param id The ID of the [Registro].
      * @param registro The [Registro] to update.
-     * @return a [Call] that can return a [Response] with a [Registro].
+     * @return a [Response] with a [Registro].
      */
     @PUT("/api/registro/{registro}")
     suspend fun updateRegistro(
         @Path("registro") id: Long,
         @Body registro: Registro
-    ): Call<Registro>
+    ): Response<Registro>
 
     /**
      * Delete a [Registro].
      *
      * @param id The ID of the [Registro].
-     * @return a [Call] that can return a [Response] with a message.
+     * @return a [Response] with a message.
      */
     @DELETE("/api/registro/{registro}")
-    suspend fun deleteRegistro(@Path("registro") id: Long): Call<ResponseBody>
+    suspend fun deleteRegistro(@Path("registro") id: Long): Response<ResponseBody>
 
 }
