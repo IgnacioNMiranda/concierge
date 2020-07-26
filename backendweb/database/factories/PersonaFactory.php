3f710be9ca3895f83bec424aaacb2e80f5815b4e
Ignacio Miranda PC <ignacio.miranda01@alumnos.ucn.cl>
2020-07-26 13:47:28 -0400

    refactor: if parentesco != 'Empresa', empresaEntrega will be always false.

--- backendweb/database/factories/PersonaFactory.php
+++ backendweb/database/factories/PersonaFactory.php
@@ -1,9 +1,10 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+/** @var Factory $factory */
 
 use App\Persona;
 use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory;
 
 $factory->define(Persona::class, function (Faker $faker) {
 
--- backendweb/database/factories/RegistroFactory.php
+++ backendweb/database/factories/RegistroFactory.php
@@ -1,18 +1,25 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+/** @var Factory $factory */
 
 use App\Registro;
 use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory;
 
 $factory->define(Registro::class, function (Faker $faker) {
 
-    $parentescos = ['Familiar', 'Externo'];
+    $parentescos = ['Familiar', 'Externo', 'Empresa'];
+    $parentesco = $parentescos[rand(0,2)];
+    if ($parentesco != "Empresa") {
+        $empresaEntrega = false;
+    } else {
+        $empresaEntrega = $faker->boolean;
+    }
 
     return [
         'fecha' => $faker->date(),
-        'parentesco' => $parentescos[rand(0,1)],
-        'empresaEntrega' => $faker->boolean,
+        'parentesco' => $parentesco,
+        'empresaEntrega' => $empresaEntrega,
         'persona_id' => rand(1, 20),
         'departamento_id' => rand(1,40),
     ];
